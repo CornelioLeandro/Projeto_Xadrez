9ad4b9097cbf72ad92a85708d6aeea9bbf9a752a
Leandro Cornelio <cornelio.leandro@gmail.com>
2020-07-07 19:45:17 -0300

    Primeira Classe: Posicao

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,12 @@
 package aplication;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Word");
+	Posicao pos = new Posicao(3, 5);
+	System.out.println(pos);
 	}
 
 }
